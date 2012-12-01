fffd3f44db13d05db59b361246a9720ae7d3a312
Yifu Diao <ydiao@04420b-dmallory.corp.zynga.com>
2012-11-30 22:55:12 -0800

    Modified Stock class

--- stock.py
+++ stock.py
@@ -1,21 +1,41 @@
 from datetime import date
 
 class Stock():
-    def __init__(self, ticker, date, p_open, p_high, p_low, p_close, volumn):
-    	self.ticker = ticker
-    	self.date = date
-    	self.p_open = p_open
-    	self.p_high = p_high
-        self.p_low = p_low
-        self.p_close = p_close
-        self.volumn = volumn
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.days = 0
+        self.dates = []
+        self.opens = []
+        self.highs = []
+        self.lows = []
+        self.closes = []
+        self.volumns = []
 
-    def __str__(self):
-    	return '%s, %s, %s, %s, %s, %s, %s' % (self.ticker, self.date, self.p_open, self.p_high, self.p_low, self.p_close, self.volumn)
+    def add_data(self, date, p_open, p_high, p_low, p_close, volumn):   	
+    	self.dates.append(date)
+    	self.opens.append(float(p_open))
+    	self.highs.append(float(p_high))
+        self.lows.append(float(p_low))
+        self.closes.append(float(p_close))
+        self.volumns.append(int(volumn))
+        self.days += 1
+    
+    def get_index_of_date(self, date):
+    	for i in range(self.days):
+    		if self.dates[i] == date:
+    			return i
+
+
+    def repr_one_day(self, index):
+    	return '%s, %s, %s, %s, %s, %s, %s' \
+    	% (self.ticker, self.dates[index], self.opens[index], self.highs[index], \
+    	  self.lows[index], self.closes[index], self.volumns[index])
 
 
 if __name__ == '__main__':
-    stock = Stock('AAPL', date(2012, 11, 30), 2.45, 2.56, 2.35, 2.41, 3000000)
-    print str(stock)    	
+    stock = Stock('ZNGA')
+    stock.add_data(date(2012, 11, 30), 2.45, 2.56, 2.35, 2.41, 3000000)
+    print stock.repr_one_day(0)
+    print stock.get_index_of_date(date(2012, 11, 30))    	
 
 
